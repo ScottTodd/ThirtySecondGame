80312d45e3a48e4d2f22b38c49602db05002fb8e
Scott Todd <scoo531@gmail.com>
2014-01-22 15:46:35 -0500

    Setup two players with wasd and arrow keys

--- TileGame/Assets/PlayerController.cs
+++ TileGame/Assets/PlayerController.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public KeyCode minusXKey;
+	public KeyCode plusXKey;
+	public KeyCode minusZKey;
+	public KeyCode plusZKey;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,16 +17,16 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		Vector3 newPosition = transform.localPosition;
 
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+		if (Input.GetKeyDown(minusXKey)) {
 			newPosition.x = newPosition.x - 1;
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+		if (Input.GetKeyDown(plusXKey)) {
 			newPosition.x = newPosition.x + 1;
 		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+		if (Input.GetKeyDown(minusZKey)) {
 			newPosition.z = newPosition.z + 1;
 		}
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+		if (Input.GetKeyDown(plusZKey)) {
 			newPosition.z = newPosition.z - 1;
 		}
 
