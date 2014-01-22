a3a22fa43f7004e48f2240fa730827b133a0d65b
Scott Todd <scoo531@gmail.com>
2014-01-22 16:18:25 -0500

    Added time to move operation, fixed key naming and configuration

--- TileGame/Assets/PlayerController.cs
+++ TileGame/Assets/PlayerController.cs
@@ -8,28 +8,53 @@ public class PlayerController : MonoBehaviour {
 	public KeyCode minusZKey;
 	public KeyCode plusZKey;
 
+	public float timeToMove = 0.5f;
+	private bool moving;
+	private float timeStartedMoving;
+	private Vector3 originalPosition;
+	private Vector3 targetPosition;
+
 	// Use this for initialization
 	void Start () {
-	
+		moving = false;
+		timeStartedMoving = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 newPosition = transform.localPosition;
+		if (!moving) {
+			if (Input.GetKey(minusXKey)) {
+				StartMoving();
+				targetPosition = new Vector3(originalPosition.x - 1, originalPosition.y, originalPosition.z);
+			}
+			if (Input.GetKey(plusXKey)) {
+				StartMoving();
+				targetPosition = new Vector3(originalPosition.x + 1, originalPosition.y, originalPosition.z);
+			}
+			if (Input.GetKey(minusZKey)) {
+				StartMoving();
+				targetPosition = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z - 1);
+			}
+			if (Input.GetKey(plusZKey)) {
+				StartMoving();
+				targetPosition = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z + 1);
+			}
+		}
 
-		if (Input.GetKeyDown(minusXKey)) {
-			newPosition.x = newPosition.x - 1;
-		}
-		if (Input.GetKeyDown(plusXKey)) {
-			newPosition.x = newPosition.x + 1;
-		}
-		if (Input.GetKeyDown(minusZKey)) {
-			newPosition.z = newPosition.z + 1;
-		}
-		if (Input.GetKeyDown(plusZKey)) {
-			newPosition.z = newPosition.z - 1;
-		}
+		if (moving) {
+			float timeDiff = Time.time - timeStartedMoving;
 
-		transform.localPosition = newPosition;
+			transform.localPosition = Vector3.Lerp(originalPosition, targetPosition, timeDiff / timeToMove);
+
+			if (timeDiff > timeToMove) {
+				moving = false;
+			}
+		}
     }
+
+	void StartMoving() {
+		timeStartedMoving = Time.time;
+		moving = true;
+		originalPosition = transform.localPosition;
+	}
 }
