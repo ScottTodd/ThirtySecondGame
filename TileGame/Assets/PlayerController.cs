125789285d483e5b5ad9aeee6eb430bda6c2289b
Scott Todd <scoo531@gmail.com>
2014-01-22 15:09:21 -0500

    Added initial Unity files, updated gitignore

---
+++ TileGame/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.localPosition;
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			newPosition.x = newPosition.x - 1;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			newPosition.x = newPosition.x + 1;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			newPosition.x = newPosition.y - 1;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			newPosition.x = newPosition.y + 1;
+		}
+
+		transform.localPosition = newPosition;
+    }
+}
