e5a467951f9d4986d810522fb9c40d275d7f6a22
Scott Todd <scoo531@gmail.com>
2014-01-24 12:33:00 -0500

    Organized assets into folders

---
+++ TileGame/Assets/Scripts/FallAfterTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallAfterTime : MonoBehaviour {
+
+    public float minTimeToFallAfter = 3.0f;
+    public float maxTimeToFallAfter = 30.0f;
+    public bool hasFallen;
+    private float timeToFallAfter;
+    private float startTime;
+
+    // Use this for initialization
+    void Start () {
+        hasFallen = false;
+        rigidbody.useGravity = false;
+        startTime = Time.time;
+        timeToFallAfter = Random.Range(minTimeToFallAfter, maxTimeToFallAfter);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Time.time - timeToFallAfter > startTime) {
+            rigidbody.useGravity = true;
+            hasFallen = true;
+        }
+    }
+}
